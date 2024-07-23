a15f141ede89cb2be0da3c603197cd6bb692a2d9
Naveen U <142392636+Naveen00222@users.noreply.github.com>
2024-07-23 17:22:24 +0530

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,11 @@
+//accept the input message in html input box and print accepted in window alert
+function accept(){
+    let value=document.getElementById("input").value;    
+    window.alert("accepted")
+    }
+    //decline the input message in html input box
+    function decline(){
+        let value=document.getElementById("input").value;
+        window.alert("declined")
+        }
+
